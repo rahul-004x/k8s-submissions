e9d4781f63814fff3b03aa79e05dbd21125d7ea8
rahul <rahulxyz123u@gmail.com>
2025-06-09 16:41:39 +0530

    chore: remove wrong structure

--- chapter1-basics/log_output/logger.js
+++
@@ -1,8 +0,0 @@
-const { v4: uuidv4 } = require('uuid')
-
-const randomString = uuidv4()
-
-setInterval(() => {
-    const timeStamp = new Date().toISOString()
-    console.log(`${timeStamp}: ${randomString}`)
-}, 5000 )
