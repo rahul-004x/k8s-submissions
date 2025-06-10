a1fd2f119728a1a636633500c881cfb2474b88d1
rahul <rahulxyz123u@gmail.com>
2025-06-10 09:57:33 +0530

    chore(manifests): for the project

--- chapter1-basics/the_project/index.js
+++ chapter1-basics/the_project/index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const path = require('path')
 const app = express()
 require('dotenv').config()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send(`<h3>Welcome to the Express Server!</h3>`)
+})
 
 const port = process.env.PORT || 3001
 app.listen(port, () => {
