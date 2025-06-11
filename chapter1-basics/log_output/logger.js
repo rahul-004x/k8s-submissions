84a363bf0abc4cf19e2a27dc80ef1db2491fbb29
rahul <rahulxyz123u@gmail.com>
2025-06-11 09:34:01 +0530

    chore(ingress): set up ingress

--- chapter1-basics/log_output/logger.js
+++ chapter1-basics/log_output/logger.js
@@ -1,8 +1,25 @@
 const { v4: uuidv4 } = require('uuid')
+const express = require('express')
+const app = express()
 
 const randomString = uuidv4()
+let messages = []
+
+const initialMessage = `${new Date().toISOString()}: ${randomString}`
+messages.push(initialMessage)
 
 setInterval(() => {
     const timeStamp = new Date().toISOString()
-    console.log(`${timeStamp}: ${randomString}`)
+    const newMessage = `${timeStamp}: ${randomString}`
+    messages.push(newMessage)
+    console.log(newMessage)
 }, 5000 )
+
+app.get('/', (req, res) => {
+    res.send(messages.join('\n'))
+})
+
+const PORT = 3003
+app.listen(PORT, () => {
+    console.log(`server is running on port ${PORT}`)
+})
